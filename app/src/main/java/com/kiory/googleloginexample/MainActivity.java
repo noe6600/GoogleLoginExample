efc727bbef837a5f602b9b5c40a35b15eb5efcba
Noemí Ferrer <noe6600@gmail.com>
2015-03-18 15:33:21 +0100

    First commit
    Google plus api connection example.

---
+++ app/src/main/java/com/kiory/googleloginexample/MainActivity.java
@@ -0,0 +1,121 @@
+package com.kiory.googleloginexample;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.CommonStatusCodes;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.GoogleApiClient.OnConnectionFailedListener;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.plus.People;
+import com.google.android.gms.plus.Plus;
+
+public class MainActivity extends ActionBarActivity
+        implements GoogleApiClient.ConnectionCallbacks, OnConnectionFailedListener, ResultCallback<People.LoadPeopleResult>{
+
+    private GoogleApiClient googlePlusClient;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.container, new PlaceholderFragment())
+                    .commit();
+        }
+
+        googlePlusClient = new GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(Plus.API, Plus.PlusOptions.builder().build())
+                .addScope(Plus.SCOPE_PLUS_LOGIN)
+                .build();
+    }
+
+    @Override
+    public void onResult(People.LoadPeopleResult peopleData) {
+        if (peopleData.getStatus().getStatusCode() == CommonStatusCodes.SUCCESS) {
+            Log.e("LoginActivity", "Connected onResult");
+        } else {
+            Log.e("LoginActivity", "Error requesting visible circles: " + peopleData.getStatus());
+        }
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+        Log.e("LoginActivity", "failed");
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+        Log.e("LoginActivity", "suspended");
+    }
+
+    @Override
+    public void onConnected(Bundle connectionHint) {
+        //Retreiving the user's email
+        String email = Plus.AccountApi.getAccountName(googlePlusClient);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        //Start connection to google plus api
+        googlePlusClient.connect();
+    }
+
+    @Override
+    protected void onStop(){
+        super.onStop();
+
+        if(googlePlusClient.isConnected()) googlePlusClient.disconnect();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    /**
+     * A placeholder fragment containing a simple view.
+     */
+    public static class PlaceholderFragment extends Fragment {
+
+        public PlaceholderFragment() {
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+            return rootView;
+        }
+    }
+}
